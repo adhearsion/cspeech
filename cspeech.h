b26bfc7e233222611bacec6d9cb6f0ae3a2b1e94
Ben Langfeld <ben@langfeld.me>
2014-01-07 14:34:11 -0200

    Use autotools for building
    #1

---
+++ cspeech.h
@@ -0,0 +1,20 @@
+/*
+ * cspeech - Speech document (SSML, SRGS, NLSML) modelling and matching for C
+ * Copyright (C) 2013, Grasshopper
+ *
+ * License: MIT
+ *
+ * Contributor(s):
+ * Chris Rienzo <chris.rienzo@grasshopper.com>
+ *
+ * cspeech.h
+ *
+ */
+
+#ifndef CSPEECH_H_
+#define CSPEECH_H_
+
+#include <cspeech/srgs.h>
+#include <cspeech/nlsml.h>
+
+#endif // CSPEECH_H_
